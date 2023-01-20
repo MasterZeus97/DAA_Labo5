eb80e2ffb19d5e737b1ad8b215fc34a979cc011b
Pascal Perrenoud <64425383+MrDaves24@users.noreply.github.com>
2023-01-20 09:35:01 +0100

    Define common interface to separate workload

--- app/src/main/java/ch/heigvd/iict/and/rest/viewmodels/ContactsViewModel.kt
+++ app/src/main/java/ch/heigvd/iict/and/rest/viewmodels/ContactsViewModel.kt
@@ -1,11 +1,11 @@
 package ch.heigvd.iict.and.rest.viewmodels
 
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import ch.heigvd.iict.and.rest.ContactsApplication
+import ch.heigvd.iict.and.rest.models.Contact
+import ch.heigvd.iict.and.rest.models.PhoneType
 import kotlinx.coroutines.launch
+import java.util.*
 
 class ContactsViewModel(application: ContactsApplication) : AndroidViewModel(application) {
 
@@ -13,18 +13,85 @@ class ContactsViewModel(application: ContactsApplication) : AndroidViewModel(app
 
     val allContacts = repository.allContacts
 
+    private val _contact = MutableLiveData<Contact?>(null)
+    val contact : LiveData<Contact?>
+        get() = _contact
+
+    fun changeContact(name: String? = null, firstname: String? = null,
+                      birthday : Calendar? = null, email: String? = null,
+                      address: String? = null, zip: String? = null,
+                      city: String? = null, type: PhoneType? = null,
+                      phoneNumber: String? = null) {
+        val c = _contact.value ?: Contact(null, "", null, null, null, null, null, null, null, null)
+
+        if (name != null) c.name = name
+        if (firstname != null) c.firstname = firstname
+        if (birthday != null) c.birthday = birthday
+        if (email != null) c.email = email
+        if (address != null) c.address = address
+        if (zip != null) c.zip = zip
+        if (city != null) c.city = city
+        if (type != null) c.type = type
+        if (phoneNumber != null) c.phoneNumber = phoneNumber
+
+        _contact.postValue(c)
+    }
+
+    /**
+     * S'enregistre au près du serveur
+     */
     fun enroll() {
         viewModelScope.launch {
             //TODO
         }
     }
 
+    /**
+     * Récupère un utilisateur
+     */
+    fun get(id: Long) {
+        // TODO : _contact.postValue(contacts.get(id))
+    }
+
+    /**
+     * Synchronise tous les contacts qui ne sont pas dans un état propre
+     */
     fun refresh() {
         viewModelScope.launch {
             //TODO
         }
     }
 
+    /**
+     * Ajoute le contact temporaire de la LiveData dans le système
+     */
+    fun insert() {
+        // TODO
+        _contact.postValue(null)
+    }
+
+    /**
+     * Met à jour le contact temporaire de la LiveData dans le système
+     */
+    fun update() {
+        // TODO
+        _contact.postValue(null)
+    }
+
+    /**
+     * Supprime le contact temporaire
+     */
+    fun delete() {
+        // TODO
+        _contact.postValue(null)
+    }
+
+    /**
+     * Détruit le contact temporaire
+     */
+    fun discardContact() {
+        _contact.postValue(null)
+    }
 }
 
 class ContactsViewModelFactory(private val application: ContactsApplication) : ViewModelProvider.Factory {
